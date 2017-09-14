4af941505e9faf8a8c634de36c3018692f1aa247
zhanyajun <yjzhang@Ctrip.com>
2017-09-14 11:22:11 +0800

    布局demo优化

--- index.ios.js
+++ index.ios.js
@@ -121,13 +121,13 @@ class ReactNativeApp extends Component {
       <View>
         <View style={{height: 20}}></View>
         <Image source={pic} style={{width: 193, height: 110}} />
-        <View><Text style={{fontWeight: 'bold'}}>I am test<Text style={{color: 'red'}}> red text</Text></Text></View>
-        <Blink style={styles.red} text="I love to blink" />
+        {/* <View><Text style={{fontWeight: 'bold'}}>I am test<Text style={{color: 'red'}}> red text</Text></Text></View> */}
+        {/* <Blink style={styles.red} text="I love to blink" />
         <Blink style={styles.bigblue} text="Yes, blinking is so great" />
         <Blink style={[styles.red, styles.bigblue]} text="Why did they ever take this out of HTML" />
-        <Blink style={[styles.bigblue, styles.red]} text="Look at me look at me look at me" />
+        <Blink style={[styles.bigblue, styles.red]} text="Look at me look at me look at me" /> */}
 
-        <View>
+        {/* <View>
           <View style={{height: 20}}></View>
           <Text>
             <Text>First part and </Text>
@@ -137,10 +137,10 @@ class ReactNativeApp extends Component {
             <Text>First part and </Text>
             <Text>second part</Text>
           </View>
-        </View>
+        </View> */}
 
-        <View style={{height: 20}}></View>
-        <View><MyAppText text='使用统一文本样式组件'/></View>
+        {/* <View style={{height: 20}}></View>
+        <View><MyAppText text='使用统一文本样式组件'/></View> */}
 
         {/* <View style={{height: 150, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
           <View style={{width: 100, height:30, backgroundColor: 'powderblue'}} />
@@ -202,12 +202,12 @@ class ReactNativeApp extends Component {
                   style={styles.item}>{item}</Text>} renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}></SectionList>
           </View> */}
 
-           <View>
+           {/* <View>
               <Text>
                   {this.state.title}
               </Text>
               <Button onPress={this.fetchNetwork} title="提交" />
-          </View>
+          </View> */}
 
           <View style={styles.container}>
             <Text style={styles.brifeText}>简介</Text>
@@ -220,31 +220,28 @@ class ReactNativeApp extends Component {
                 </View>
                 <View style={styles.movieActor}>
                   <Text style={styles.movieActorTitle}>主演：山下智久</Text>
-                  <Text style={styles.movieActorText}>杨颖</Text>
                 </View>
                 <View style={styles.movieType}>
                   <Text style={styles.movieTypeTitle}>类型：日漫，热血</Text>
-                  <Text style={styles.movieTypeText}>可爱</Text>
                 </View>
                 <View style={styles.movieArea}>
                   <Text style={styles.movieAreaTitle}>地区：日本</Text>
-                  <Text style={styles.movieAreaText}>出版地区：中国</Text>
                 </View>
                 <View style={styles.movieYear}>
-                  <Text style={styles.movieYearTitle}>年代：1999</Text>
-                  <Text style={styles.movieYearText}>发行年代：2001</Text>
+                  <Text style={styles.movieYearTitle}>年代：</Text>
+                  <Text style={styles.movieYearText}>2001</Text>
                 </View>
                 <View style={styles.movieSource}>
                   <View style={styles.movieSource2}>
                     <Text style={styles.movieSourceTitle}>评分：</Text>
                     <Text style={styles.movieSourceText}>9.1</Text>
                   </View>
-                  <Button onPress={this._onPress.bind(this)} style={styles.movieSourceButton} color='#ff9313' accessibilityLabel='Learn more about purple'>
+                  {/* <Button onPress={this._onPress.bind(this)} style={styles.movieSourceButton} color='#ff9313' accessibilityLabel='Learn more about purple'>
                     我要评分
-                  </Button>
+                  </Button> */}
                 </View>
               </View>
-              <View>
+              <View style={styles.chileFlex}>
                 <Text style={styles.detailText}>有个男人他拥有世界上切财富、名望和权势，他就是「海盗王」高路德•罗杰。 在临死前说过这样一句话：让全世界的人都奔向大海 「想要我的财宝吗？想要的话全就拿去吧……！你们去找吧！我把世界上的一切都放在那里了」。 后来世界上的人们将这个宝藏称作“一个大秘宝”（One Piece），许多人为了争夺大秘宝One Piece，无数海盗扬起旗帜，互相斗争，后来就形成了「大海盗时代」。 主角蒙奇•D•路飞在遥远的路途上找寻着志同道合的伙伴，携手共进「伟大航线」，目标当上「海盗王」。</Text>
               </View>
             </View>
@@ -289,19 +286,21 @@ const styles = StyleSheet.create({
     moviewBrifeContatiner: {
       flexDirection: 'row',
       justifyContent: 'flex-start',
-      marginLeft: 12,
-      marginRight: 12,
+      marginLeft: 6,
+      marginRight: 6,
       marginTop: 4,
     },
     movieImage: {
       width: 100,
       aspectRatio: 0.758,
+      backgroundColor: '#ff0000',
+      flex: 1,
     },
     movieInfo: {
       flexDirection: 'column',
       justifyContent: 'flex-start',
       flex: 1,
-      marginLeft: 12,
+      marginLeft: 0,
     },
     movieDetail: {
       flexDirection: 'column',
@@ -363,7 +362,7 @@ const styles = StyleSheet.create({
       flexDirection: 'row',
       justifyContent: 'flex-start',
       marginTop: 5,
-    }，
+    },
     movieYearTitle: {
       fontSize: 11,
       color: '#646464',
@@ -374,7 +373,7 @@ const styles = StyleSheet.create({
     },
     movieSource: {
       flexDirection: 'row',
-      justifyContent: 'space-between',
+      justifyContent: 'flex-start',
       flexGrow: 1,
     },
     movieSource2: {
@@ -401,6 +400,12 @@ const styles = StyleSheet.create({
       marginTop: 16,
       marginBottom: 16,
       color: '#646464',
+      flexShrink:1,
+    },
+    chileFlex: {
+        //width: 100,
+        backgroundColor: '#00ff00',
+        flex: 2,
     },
 });
 
