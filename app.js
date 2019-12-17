a8774a7cc46ed3285078e347114fc948d0e51c61
zhanyajun <yjzhang@Ctrip.com>
2019-12-17 14:41:32 +0800

    change app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// import AppHome from './src/page/Page1'
-import AppHome from './src/bill/ZhangBenHomeView'
+import AppHome from './src/page/Page1'
+// import AppHome from './src/bill/ZhangBenHomeView'
 
 module.exports = AppHome;
